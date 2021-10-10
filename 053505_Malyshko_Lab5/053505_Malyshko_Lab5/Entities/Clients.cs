fd2dfed07b659ddfec2166c54d3b7f7d928cbc6d
Angelma112358 <79206480+Angelina112358@users.noreply.github.com>
2021-10-11 00:11:28 +0300

    Add files via upload

---
+++ 053505_Malyshko_Lab5/053505_Malyshko_Lab5/Entities/Clients.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _053505_Malyshko_Lab5.Entities
+{
+    class Clients
+    {
+        public readonly Dictionary<int, Tariffs> IndividualTariffs = new();
+        private int generalTraffic = 0;
+        public int GeneralTraffic
+        {
+            get 
+            {
+                generalTraffic = 0;
+                generalTraffic = IndividualTariffs.Sum(n => n.Value.Price);
+                return generalTraffic;
+            }
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Clients() { }
+        public Clients(string name, string surname, Dictionary<int, Tariffs> tariff)
+        {
+
+            Name = name;
+            Surname = surname;
+            IndividualTariffs = tariff;
+        }
+
+        public IEnumerable<(string Name, int Sum)> GetByTariff()
+        {
+            return IndividualTariffs.GroupBy(t => t.Value.Name)
+                .Select(g => (g.Key, g.Sum(t => t.Value.Price))).ToList();
+        }
+
+        public override string ToString()
+        {
+            string info = Surname + "\t" + Name;
+            for (int i = 0; i < IndividualTariffs.Count; i++)
+                info += IndividualTariffs[i] + "\t" + "\t" + "\n\t";
+            return info + "\t\t\t\t" + GeneralTraffic;
+        }
+    }
+}
---
+++ 053505_Malyshko_Lab5/053505_Malyshko_Lab5/Entities/MobileStation.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+namespace _053505_Malyshko_Lab5.Entities
+{
+
+    class MobileStation
+    {
+        public delegate void Handler(string message, string name);
+        public event Handler Notify;
+
+        private readonly Dictionary<int, Tariffs> Tariffs = new();
+        private readonly List<Clients> Clients = new();
+
+        public void AddTariff(string Name, int Price)
+        {
+            Tariffs tariff = new(Name, Price, Tariffs.Count + 1);
+            Tariffs.Add(Tariffs.Count + 1, tariff);
+            Notify?.Invoke("Добавлен тариф: ", Name);
+        }
+
+        public void AddClients(string name, string surname, int number)
+        {
+            Clients client = new();
+            client.Name = name;
+            client.Surname = surname;
+            client.IndividualTariffs.Add(client.IndividualTariffs.Count, Tariffs[number]);
+            Clients.Add(client);
+            Notify?.Invoke("Добавлен клиент: ", surname + " " + name);
+        }
+
+        public void AddTraffic(string Surname, int number)
+        {
+            for (int i = 0; i < Clients.Count; i++)
+            {
+                if (Equals(Clients[i].Surname, Surname))
+                {
+                    Clients[i].IndividualTariffs.Add(Clients[i].IndividualTariffs.Count, Tariffs[number]);
+                    Notify?.Invoke("Добавлен траффик по тарифу: " + Tariffs[number].Name, "Кому: " + Surname);
+                }
+            }
+        }
+
+        public IEnumerable<Clients> MaxTraffic()
+        {
+            var a = Clients.Max(t => t.GeneralTraffic);
+            return from i in Clients
+                   where i.GeneralTraffic == Clients.Max(t => t.GeneralTraffic)
+                   select i;
+        }
+
+        public void RemoveTariff(int number)
+        {
+            Tariffs.Remove(number);
+        }
+
+        public void RemoveClients(string Surname)
+        {
+            foreach (Clients client in Clients)
+                if (client.Surname == Surname)
+                    Clients.Remove(client);
+        }
+
+        public List<Clients> GetClients()
+        {
+            return Clients;
+        }
+
+        public Dictionary<int, Tariffs> GetTariffs()
+        {
+            return Tariffs;
+        }
+
+        public int AmountClients(int amount)
+        {
+            int size = Clients.Aggregate(0, (a, p) => { return a = p.GeneralTraffic > amount ? a + 1 : a; });
+            return size;
+        }
+
+        public int AllCost()
+        {
+            int Sum = Clients.Sum(n => n.GeneralTraffic);
+            return Sum;
+        }
+
+        public IEnumerable<(string, int)> ListOfSum(string Surname)
+        {
+            IEnumerable<(string, int)> client = null;
+            for (int i = 0; i < Clients.Count; i++)
+                if (Clients[i].Surname == Surname)
+                    client = Clients[i].GetByTariff();
+            if (client != null)
+                return client;
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
