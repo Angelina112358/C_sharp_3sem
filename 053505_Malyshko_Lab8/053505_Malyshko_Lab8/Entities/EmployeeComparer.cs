95f075d35d4ced5efe2ca65e6d6f05ed656c329f
Angelma112358 <79206480+Angelina112358@users.noreply.github.com>
2021-10-11 00:51:04 +0300

    Add files via upload
    Lab 8 Файлы и потоки данных

---
+++ 053505_Malyshko_Lab8/053505_Malyshko_Lab8/Entities/EmployeeComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _053505_Malyshko_Lab8.Entities
+{
+    class EmployeeComparer:IComparer<Employee>
+    {
+        public int Compare(Employee? x, Employee? y) =>
+            string.Compare(x?.Name, y?.Name, StringComparison.InvariantCulture);
+    }
+}
---
+++ 053505_Malyshko_Lab8/053505_Malyshko_Lab8/Interfaces/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _053505_Malyshko_Lab8.Interfaces
+{
+    interface IFileService
+    {
+        public IEnumerable<Employee> ReadFile(string fileName);
+        public void SaveData(IEnumerable<Employee> data, string fileName);
+    }
+}
