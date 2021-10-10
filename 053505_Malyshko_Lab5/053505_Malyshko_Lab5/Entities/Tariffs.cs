8d4f1f78738bd77c6036515b55103087efaf6f7d
Angelma112358 <79206480+Angelina112358@users.noreply.github.com>
2021-10-11 00:45:33 +0300

    README.md
    Lab 6 События, исключения
    (Описать класс Journal)
    Lab 7 Стандартные коллекции. Технология LINQ

---
+++ 053505_Malyshko_Lab5/053505_Malyshko_Lab5/Entities/Tariffs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+
+namespace _053505_Malyshko_Lab5.Entities
+{
+    class Tariffs
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        readonly int Number;
+        public Tariffs() { }
+        public Tariffs(string name, int price, int number)
+        {
+            Name = name;
+            Price = price;
+            Number = number;
+        }
+        public override string ToString()
+        {
+            return Number.ToString() + "\t" + Name.ToString() + "\t" + Price.ToString();
+        }
+    }
+}
